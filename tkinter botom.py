ca6429dc4e12f02b6eea5a5a9c594ee4e304e110
YYCCFF <46415118+YYCCFF@users.noreply.github.com>
2019-02-21 23:43:11 +0900

    Add files via upload

---
+++ tkinter botom.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+def pushed(self):
+    self["text"]="押されたよ"
+
+root = tk.Tk()
+root.geometry("320x240")
+root.title("tkinter test")
+
+label = tk.Label(root,text="Tkinterのテストです")
+label.grid()
+
+button = tk.Button(root, text="ボタン", command= lambda : pushed(button))
+button.pack()
+
+root.mainloop()
