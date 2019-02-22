d4cbbadce8c43415ae5ea130f0b243fd9f0c951d
YYCCFF <46415118+YYCCFF@users.noreply.github.com>
2019-02-23 02:51:19 +0900

    Add files via upload

---
+++ Label.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter import font
+root = tk.Tk()
+root.title("Label")
+root.geometry("300x300+1000+100")
+label1 = tk.Label(root,text="Hello")
+label1.pack(side="top")
+font1 = font.Font(family="Helvetica",size=20,weight="bold")
+label2 = tk.Label(root,text="Bye",bg="violet",font=font1)
+label2.pack(side="top")
+font2 = font.Font(family="Times",size=40)
+label2 = tk.Label(root,text="See you",fg="red",font=font2)
+label2.pack(side="top")
+root.mainloop()
