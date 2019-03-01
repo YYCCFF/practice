dad74eea5b4e74eb49a9c6251c331d6695764208
YYCCFF <46415118+YYCCFF@users.noreply.github.com>
2019-03-02 01:33:49 +0900

    Add files via upload

---
+++ for.py
@@ -0,0 +1,8 @@
+#python
+movies={"Your Name":107,
+        "Home Alone":103,
+        "Harry Potter and the Philosopher's Stone":159,}
+
+for movie_name in movies:
+    print("The screening time of the "+movie_name+" is "+str(movies[movie_name])+" minutes.")
+
