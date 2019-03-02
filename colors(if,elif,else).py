0fd1658732c807c7e37713e6724a910fea1fbd11
YYCCFF <46415118+YYCCFF@users.noreply.github.com>
2019-03-03 01:18:04 +0900

    Add files via upload

---
+++ colors(if,elif,else).py
@@ -0,0 +1,10 @@
+#python
+colors={"red","yellow","blue"}
+
+#You choice two colors
+if("red" and "yellow"):
+    print("Mixing red and yellow makes orange it.")
+elif("red" and "blue"):
+    print("Mixing red and yellow makes purple it.")
+else:
+    print("Mixing yellow and bule makes green it.")
